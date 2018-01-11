523a76efab7e6668ece6341b9f6f028efb016619
antteng <antteng@163.com>
2018-01-11 22:12:08 +0800

    add header

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,15 @@
 <template>
-  <div>index</div>
+  <index-header></index-header>
 </template>
 
 <script>
-export default {
-  name: 'index'
-}
+  import IndexHeader from './header'
+  export default {
+    name: 'index',
+    components: {
+      IndexHeader
+    }
+  }
 </script>
+<style></style>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

a07204c3573ea24269579bf83f49158a5ca0c979
bai <bjj2056654006@163.com>
2018-01-13 23:07:47 +0800

    aaa

--- src/pages/tour/index.vue
+++ src/pages/tour/index.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>tour</div>
+  <div>
+    <tour-header></tour-header>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'tour'
+  import TourHeader from './tourheader'
+  name: 'tour',
+  components: {
+    TourHeader
+  }
 }
 </script>
-
+  
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

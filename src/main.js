d65a220756467f8e25388838f634f95de898faff
lj <951551408@qq.com>
2018-01-13 21:42:06 +0800

    index

--- src/main.js
+++ src/main.js
@@ -3,9 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import VueLazyload from 'vue-lazyload'
 import 'assets/css/reset.css'
+import 'assets/css/iconfont/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
+FastClick.attach(document.body)
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'http://i04.pic.sogou.com/64ec42661f81d0b0',
+  loading: 'http://img.zcool.cn/community/01439357dd73c00000018c1b3e04ce.gif',
+  attempt: 1
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,116 @@
 <template>
-  <div>index</div>
+  <div class="main">
+  	<index-header></index-header>
+    <index-swiper :list="swiperInfo"></index-swiper>
+    <index-icons :list="iconsInfo"></index-icons>
+    <div class="city-location">
+      <div class="city-left iconfont">&#xe63f;定位失败</div>
+      <div class="city-right iconfont">&#xe811;5折泡温泉</div>
+    </div>
+    <div class="city-shop"><p class="shove">热销推荐</p></div>
+    <IndexScroller class="scroller" :sights="sightsInfo"></IndexScroller>
+    <div class="all-shop">
+      <router-link to="/">查看所有产品</router-link><!-- 标签 -->
+    </div>
+    <index-travel :travel="travelInfo"></index-travel>
+    <index-blogroll></index-blogroll>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import IndexScroller from './scroller'
+import IndexHeader from './header'
+import IndexSwiper from './swiper'
+import IndexIcons from './icons'
+import IndexTravel from './travel'
+import IndexBlogroll from './blogroll'
 export default {
-  name: 'index'
+  name: 'index',
+  components: {
+    IndexHeader,
+    IndexSwiper,
+    IndexIcons,
+    IndexScroller,
+    IndexTravel,
+    IndexBlogroll
+  },
+  data () {
+    return {
+      swiperInfo: [],
+      iconsInfo: [],
+      sightsInfo: [],
+      travelInfo: []
+    }
+  },
+  methods: {
+    getIndexData () {
+      axios.get('/api/index.json')
+      .then(this.handleGetDataSucc.bind(this))
+      .catch(this.handleGetDataErr.bind(this))
+    },
+    handleGetDataSucc (res) {
+      const data = res.data.data
+      this.swiperInfo = data.swiperList
+      this.iconsInfo = data.iconList
+      this.sightsInfo = data.sights
+      this.travelInfo = data.cityList
+    },
+    handleGetDataErr () {
+      console.log('error')
+    }
+  },
+  created () {
+    this.getIndexData()
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+@import "../../assets/stylus/varibles.styl";
+  .main
+    display: flex
+    flex-direction: column
+    // position: absolute
+    // left: 0
+    // right: 0
+    // bottom: 0
+    // top: 0
+    .scroller
+      // flex: 1
+      height: 5.76rem
+      overflow: hidden
+    .city-location
+      display: flex
+      width:100%
+      margin-top: .1rem
+      border-top: .01rem solid #f0f0f0
+      background-color: #fff
+      .city-left
+        width: 3.7rem
+        height: .98rem
+        line-height: .98rem
+        font-size: .28rem
+        text-align: center
+        border-right: .01rem solid #f0f0f0
+        color: #212121
+      .city-right
+        width: 3.8rem
+        height: .98rem
+        line-height: .98rem
+        font-size: .28rem
+        text-align: center
+        color: #212121
+    .city-shop
+      width: 100%
+      height: .8rem
+      line-height: .8rem
+      background: $longStrip
+      .shove
+        margin-left: .26rem
+    .all-shop
+      height: .88rem
+      line-height: .88rem
+      text-align: center
+      color: #00afc7
 </style>
